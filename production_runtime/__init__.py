dd135ceb51e303040be578061785eab41514e586
Wei <contact@wei-dev.com>
2026-08-18 09:21:48 +0200

    feat(runtime): expose production runtime facade

---
+++ production_runtime/__init__.py
@@ -0,0 +1,23 @@
+from .contracts import (
+    MECHANISM_CONTEXT_STAGE,
+    PRODUCTION_BUNDLE_SCHEMA,
+    PRODUCTION_EXECUTION_SCHEMA,
+    PRODUCTION_MECHANISMS,
+    PRODUCTION_STAGE_RESULT_SCHEMA,
+    PRODUCTION_STATUS_SCHEMA,
+    ProductionRunError,
+)
+from .runtime import ProductionRunExecutor
+from .sources import ProjectContextSourceLoader
+
+__all__ = [
+    "MECHANISM_CONTEXT_STAGE",
+    "PRODUCTION_BUNDLE_SCHEMA",
+    "PRODUCTION_EXECUTION_SCHEMA",
+    "PRODUCTION_MECHANISMS",
+    "PRODUCTION_STAGE_RESULT_SCHEMA",
+    "PRODUCTION_STATUS_SCHEMA",
+    "ProductionRunError",
+    "ProductionRunExecutor",
+    "ProjectContextSourceLoader",
+]
